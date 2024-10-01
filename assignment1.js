032e1e69119415c4621e8a5be23b28c3ae0bece9
bilalahmedtahir <bilalahmedtahird18@gmail.com>
2024-10-01 17:12:51 +0500

    asdas

---
+++ assignment1.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Define a route
+app.get("/", (req, res) => {
+  res.status(200).send({
+    id: 500,
+    name: "Abdullah",
+    tech: "FrontEnd",
+  });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
