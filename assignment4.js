e6d46cfa99da16f38a5fdd6c9f580d0f7079cec1
Abdullah Ashar <aasheikh@nisum.com>
2024-10-02 18:49:43 +0500

    assignment 4 and 5

---
+++ assignment4.js
@@ -0,0 +1,14 @@
+for(let i = 1;i<100;i++){
+    if(i%2 && i%5 === 0){
+        console.log("both");
+    }
+    else if(i%2 === 0){
+        console.log("two");
+    }
+    else if(i%5 === 0){
+        console.log(five);
+    }
+    else{
+        console.log("neither");
+    }
+}
